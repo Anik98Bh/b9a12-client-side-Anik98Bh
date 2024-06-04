845552ee5fa2da696b45b21f3cfb6409b62b08a7
Anik98B <anikbhowmik0188@gmail.com>
2024-06-04 19:05:51 +0600

    create auth provider

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,25 @@
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import { createContext, useState } from "react";
+import { app } from "../firebase/firebase.config";
+
+export const AuthContext = createContext(null);
+
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const googleProvider = new GoogleAuthProvider();
+
+    const authInfo = {
+        user,
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
